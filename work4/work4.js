d3def8036aa36b3fb0afc031879ace0ec56e3592
apple <apple@jd505-33.local>
2019-10-12 12:28:33 +0800

    work4

--- work4/work4.js
+++ work4/work4.js
@@ -1,13 +1,13 @@
 document.write("<p>金字塔</p>")
 let level = prompt('请设置金字塔的层数')
-leve = parseFloat(level)&&Number(level)
+level = parseFloat(level)&&Number(level)
 if (isNaN(level)){
     alert('金字塔的层数是数字')
 }
 for(let i=1;i<=level;++i){
     let blank = level - i;
-    for(let kk=0;k<blank;++k){
-        document.write('&nbsp')
+    for(let k=0;k<blank;++k){
+        document.write('&nbsp;')
     }
     //打印星星
     let star = i * 2 -1
@@ -22,8 +22,8 @@ let str = '<table border="1">'
 for(let i=1;i<10;++i){
     str += '<tr>'
     for(var j=1;j<=i;++j){
-    //;拼接单元格
-    str += '<td>' + j + '*' + i + '=' + (j * i) + '</td>'
+        //;拼接单元格
+        str += '<td>' + j + '*' + i + '=' + (j * i) + '</td>'
     }
     str += '</tr>'
 }
