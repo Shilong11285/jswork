59f91721871527badb462ed45ea8034dc8828968
Shilong11285 <471775385@qq.com>
2019-10-18 00:53:16 +0800

    work6

--- work6/pai.js
+++ work6/pai.js
@@ -1,35 +1,49 @@
 //“计算"按钮点击处理函数
 function calculate(){ 
-  //读取阶散输入框的数值
+    //读取阶散输入框的数值
     let num = document.getElementById('num').value 
     num = parseInt(num)&&Number(num) 
-  if(!isNaN(num)){
-    //设置圆周率输入框的值   
-    document.getElementById('pai').value = pi(num) 
+    if(!isNaN(num)){
+      //设置圆周率输入框的值   
+      document.getElementById('pai').value = pi(num) 
+    } 
   } 
-} 
-    function factorial(n){ 
+  //声明阶乘函数n！  factorial（）
+  function factorial(n){ 
+    //声明阶乘汇总数并赋值为1
     let sum = 1 
-  for(let i=1;i<=n;++i){ 
-   sum *= i 
+    //for循环累乘
+    for(let i=1;i<=n;++i){ 
+      sum *= i 
+    } 
+    //还回阶乘汇总数
+    return sum 
   } 
-  return sum 
-} 
-    function oddFactorial(n){ 
+  //声名奇数阶乘函数（2n+1）！！oddFactorial（）
+  function oddFactorial(n){ 
+    //声明阶乘汇总并赋值为1
     let sum = 1 
-  for(let i=1;i<=n;++i){ 
-    sum *= 2*i+1 
+    //for循环累乘
+    for(let i=1;i<=n;++i){ 
+      sum *= 2*i+1 
+    } 
+    //还回阶乘汇总数
+    return sum 
   } 
-  return sum 
-} 
-    function pi(num){ 
+  //阶乘法计算圆周率函数
+  function pi(num){ 
+    //声明阶乘汇总并赋值为0
     let sum = 0 
-  for(let i=0;i<=num;++i){ 
-    let dividend = factorial(i) 
-    let divisor = oddFactorial(i) 
-    sum += (dividend/divisor) 
+    //for循环计算圆周率
+    for(let i=0;i<=num;++i){
+      //声明阶乘被除数 divdend，并调用factorial函数取值 
+      let dividend = factorial(i) 
+      //声明阶乘被除数 divisor，并调用oddFactorial函数取值 
+      let divisor = oddFactorial(i) 
+      //累加汇总值
+      sum += (dividend/divisor) 
+    } 
     //返回圆周率
+    return sum*2 
   } 
-  return sum*2 
-} 
-  calculate() 
+calculate() 
