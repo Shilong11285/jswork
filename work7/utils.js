f006b048eec7dcb9f248a3535ed24b66d796e7e9
Shilong11285 <471775385@qq.com>
2019-10-21 14:04:58 +0800

    work7

--- work7/utils.js
+++ work7/utils.js
@@ -1,20 +1,20 @@
-function bubbleSort(str) {
-    let arr = scr.split(',')
-    for (let i = 0; i < arr.lenght - 1; i++) {
-        for (let j = 0; j < arr.lenght - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+function bubbleSort(str){
+    let arr = str.split(',')
+    for(let i =0;i<arr.length-1;i++){
+        for(let j = 0;j<arr.length-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
             }
         }
     }
     return arr
 }
-function bubbleSort(str) {
+function insertSort(str){
     let arr = str.split(',')
-    for (let i = 1; i < arr.lenght - 1; i++) {
-        for (let j = i; j > 0; j--) {
-            if (arr[ - 1] > arr[j]) {
-                [arr[ - 1], arr[j]] = [arr[j], arr[j - 1]]
+    for (let i=1;i<arr.length;i++){
+        for (let j= i;j>0;j--){
+            if (arr[j-1]>arr[j]){
+            [arr[j-1],arr[j]]=[arr[j],arr[j-1]]
             }
         }
     }
