94d1ab06b78ba6a2562565817f397758e62a12b5
Shilong11285 <471775385@qq.com>
2019-12-29 23:16:28 +0800

    work19

--- work19/index.js
+++ work19/index.js
@@ -1,31 +1,30 @@
 let ws
-function connect(){
+function connect() {
     let server = document.getElementById('server').value
     ws = new WebSocket(server);
-    ws.onopen = function (){  
-        document.getElementById('conn').disabled='disable';
-        document.getElementById('disconn').disabled='';
-        let nickname=document.getElementById('nickname').value
+    ws.onopen = function(){
+        document.getElementById('conn').disabled = 'disable';
+        document.getElementById('disconn').disabled = '';
+        let nickname = document.getElementById('nickname').value
         if(nickname){
             ws.send('nickname|'+nickname)
         }
-
     }
-    ws.onclose = function() {
-        document.getElementById('conn').disable='';
-        document.getElementById('disconn').disable= 'disable';
+    ws.onclose = function(){
+        document.getElementById('conn').disable = '';
+        document.getElementById('disconn').disable = 'disable';
     }
-    ws.onclose = function(event) {
+    ws.onmessage = function(event){
         let board = document.getElementById('board')
         let newmsg = document.createElement('div')
         console.log(event.data)
-        newmsg.innerHTML= event.data
+        newmsg.innerHTML = event.data
         board.appendChild(newmsg)
         board.scrollTop = board.scrollHeight;
     }
 }
 
-function disconnect(){
+function disconnect (){
     ws.close()
 }
 
