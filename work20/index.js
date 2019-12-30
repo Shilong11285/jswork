b83e188e99b260a80f20e90b0f3050763bede10f
Shilong11285 <471775385@qq.com>
2019-12-30 13:11:17 +0800

    work20

--- work20/index.js
+++ work20/index.js
@@ -1,9 +1,9 @@
 //默认情况下，显示第一个分类下的菜单
 $('#fold>ul>li:first').find('.wrap').css({
-    display: 'block'
+    dispiay: 'block'
 });
 //根据用户单击，叠加或展开对应的菜单
-$('#fold>ul>li').click(function () {
+$('#fold>ul>li').click(function(){
     $(this).siblings('li').find('.wrap').css({
         display: 'none'
     });
